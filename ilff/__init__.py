b6b640d37a287a24604d3b6c4d70c5f2f9debe40
Johannes Willkomm <j.willkomm@fionec.de>
2024-09-25 10:36:19 +0200

    Add ilffGetIndex, unlink, less exports, Exceptions on invalid seek

--- ilff/__init__.py
+++ ilff/__init__.py
@@ -2,6 +2,6 @@ __version__ = '0.7.0'
 
 VERSION = __version__
 
-from ilff.ilff import *
+from ilff.ilff import ILFFFile, ILFFGetLines, unlink
 from . import reindex
 from ilff.cilff import CILFFFile
--- src/ilff.h
+++ src/ilff.h
@@ -14,6 +14,8 @@ int ilffGetLine(ILFFFile *, int64_t lnnum, char *data, int64_t *nChars);
 int ilffGetLines(ILFFFile* ilff_, int64_t const lnnum, int64_t const N, char** data, int64_t* lengths);
 int ilffGetRange(ILFFFile *, int64_t const lnnum, int64_t const N, char *data, int64_t *maxLen);
 
+int ilffGetIndex(ILFFFile *, int64_t lnnum, int64_t const N, int64_t* index);
+
 int64_t ilffNLines(ILFFFile *);
 
 int ilffReindex(ILFFFile*);
