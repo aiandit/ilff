2a39dac2f81a62616e50e2ab2a9b7e0bd94ff945
Johannes Willkomm <johannes@johannes-willkomm.de>
2024-11-23 20:00:34 +0100

    aiofile may not exist
    Gbp-Pq: Name 0001-aiofile-may-not-exist.patch

--- ilff/__init__.py
+++ ilff/__init__.py
@@ -3,7 +3,11 @@ __version__ = '25.5'
 VERSION = __version__
 
 from ilff.ilff import ILFFFile, unlink, open
-from ilff.ailff import AILFFFile, open as async_open
+try:
+    import aiofiles
+    from ilff.ailff import AILFFFile, open as async_open
+except:
+    pass
 from ilff.cilff import CILFFFile
 from ilff.ilffgetlines import ILFFGetLines, CILFFGetLines
 from . import reindex
