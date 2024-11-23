8379c50b6a6c33462cdaf0fe20b9b2667b8aec53
Johannes Willkomm <johannes@johannes-willkomm.de>
2024-11-23 16:56:21 +0100

    Add async version using aiofile

--- ilff/__init__.py
+++ ilff/__init__.py
@@ -3,6 +3,7 @@ __version__ = '0.8.2'
 VERSION = __version__
 
 from ilff.ilff import ILFFFile, unlink, open
+from ilff.ailff import AILFFFile, open as async_open
 from ilff.cilff import CILFFFile
 from ilff.ilffgetlines import ILFFGetLines, CILFFGetLines
 from . import reindex
